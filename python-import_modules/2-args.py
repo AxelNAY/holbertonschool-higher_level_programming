fcf7ee7c71fcb8db94e02387809211f3999c016d
AxelNAY <axelnay@sfr.fr>
2024-05-10 12:06:29 +0000

    destroy the paranthese for the if condition in the file 2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 if __name__ == "__main__":
     from sys import argv
     number = len(argv) - 1
-    if (number == 0):
+    if number == 0:
         print("0 arguments.")
-    elif (number == 1):
+    elif number == 1:
         print("1 argument:")
     else:
         print("{} arguments:".format(number))
