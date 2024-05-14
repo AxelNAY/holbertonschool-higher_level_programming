1f6edb9b85c683a28883fe9bd92c946ccd06e608
AxelNAY <axelnay@sfr.fr>
2024-05-14 08:31:16 +0000

    Complete the task 6 to 8

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i])):
-            print("{:d}".format(matrix[i]), end="")
-            if j != len(matrix[i]) - 1:
-                print(end=" ")
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end="")
+            if i != len(row) - 1:
+                print(" ", end="")
         print()
