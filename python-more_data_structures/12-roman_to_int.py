1ca7aaee140f572bf0c0144e848e3651e3b379c1
AxelNAY <axelnay@sfr.fr>
2024-05-17 08:15:04 +0000

    Complete the file 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -5,17 +5,32 @@ def roman_to_int(roman_string):
     numb = 0
     for i in range(len(roman_string)):
         if roman_string[i] == "I":
-            if i != len(roman_string) - 1 and roman_string[i + 1] != "V" or roman_string[i + 1] != "X":
+            if i != len(roman_string) - 1:
+                if roman_string[i + 1] != "V" and roman_string[i + 1] != "X":
+                    numb += 1
+                else:
+                    numb -= 1
+            else:
                 numb += 1
         elif roman_string[i] == "V":
             numb += 5
         elif roman_string[i] == "X":
-            if i != len(roman_string) - 1 and roman_string[i + 1] != "L" or roman_string[i + 1] != "C":
+            if i != len(roman_string) - 1:
+                if roman_string[i + 1] != "L" and roman_string[i + 1] != "C":
+                    numb += 10
+                else:
+                    numb -= 10
+            else:
                 numb += 10
         elif roman_string[i] == "L":
             numb += 50
         elif roman_string[i] == "C":
-            if i != len(roman_string) - 1 and roman_string[i + 1] != "D" or roman_string[i + 1] != "M":
+            if i != len(roman_string) - 1:
+                if roman_string[i + 1] != "D" or roman_string[i + 1] != "M":
+                    numb += 100
+                else:
+                    numb -= 100
+            else:
                 numb += 100
         elif roman_string[i] == "D":
             numb += 500
