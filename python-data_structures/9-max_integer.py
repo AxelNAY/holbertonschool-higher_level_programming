970a6e82b53ba38384b86f8d6f7ad16624906a7b
AxelNAY <axelnay@sfr.fr>
2024-05-18 21:03:47 +0000

    Complete all the unfinish files

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,6 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    numb = 0
-    for i in range(0, len(my_list)):
-        if my_list[i] > numb:
-            numb = my_list[i]
-    return numb
+    else:
+        my_list.sort(reverse=True)
+        return (my_list[0])
