8ff80253ed58a250cf2016593d475ad98ba56b32
AxelNAY <axelnay@sfr.fr>
2024-05-18 16:28:12 +0000

    Complete the file 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -56,11 +56,12 @@ class Square:
         """print a square with #"""
         if self.__size == 0:
             print()
-        else:
-            [print("") for i in range(self.__position[1])]
-            for i in range(self.__size):
-                for j in range(self.__position[0]):
-                    print(" ", end="")
-                for k in range(self.__size):
-                    print("#", end="")
-                print()
+            return
+        [print("") for i in range(self.__position[1])]
+
+        for i in range(self.__size):
+            for j in range(self.__position[0]):
+                print(" ", end="")
+            for k in range(self.__size):
+                print("#", end="")
+            print()
