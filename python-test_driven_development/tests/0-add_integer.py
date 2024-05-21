8129d1485d3e0b989d3430eb03921622fd5df4c3
AxelNAY <axelnay@sfr.fr>
2024-05-21 10:05:20 +0000

    Complete the file 0-add_integer.py in the tests directory

--- python-test_driven_development/tests/0-add_integer.py
+++ python-test_driven_development/tests/0-add_integer.py
@@ -16,71 +16,29 @@ Test_1 adding two integers
     >>> add_integer(2, 1)
     3
 
-Test_2 adding float with integer cast to the small int value
+Test_2 adding two integer with b being a negative value
 -------------------------------------------------------------
-    >>> add_integer(3.2, 2)
-    5
+    >>> add_integer(100, -2)
+    98
 
-Test_3 adding int with str
+Test_3 passing only one argument with b initiate to 98
 --------------------------
-    >>> add_integer("3", 4)
-    Traceback (most recent call last):
-    TypeError: a must be an integer
+    >>> add_integer(2)
+    100
 
-Test_4 adding str with int
+Test_4 adding a float with a negative integer
 --------------------------
-    >>> add_integer(3, "4")
+    >>> add_integer(100.3, -2)
+    98
+
+Test_5 adding an integer with a string
+--------------------------
+    >>> add_integer(4, "School")
     Traceback (most recent call last):
     TypeError: b must be an integer
-
-Test_5 adding None with int
---------------------------
-    >>> add_integer(None, 4)
-    Traceback (most recent call last):
-    TypeError: a must be an integer
 
 Test_6 adding just first parameter as None
 ------------------------------------------
     >>> add_integer(None)
-    Traceback (most recent call last):
-    TypeError: a must be an integer
-
-Test_7 adding negative numbers
--------------------------------
-    >>> add_integer(-3, -4)
-    -7
-
-Test_8 adding negative float numbers
--------------------------------------
-    >>> add_integer(-3.4, -4.6)
-    -7
-
-Test_9 second argument as None
--------------------------------------
-    >>> add_integer(-3.4, None)
-    Traceback (most recent call last):
-    TypeError: b must be an integer
-
-Test_10 both arguments as strings
--------------------------------------
-    >>> add_integer("-3.4", "3")
-    Traceback (most recent call last):
-    TypeError: a must be an integer
-
-Test_11 more than two arguments
-------------------------------------
- >>> add_integer(3, 234, 65)
-    Traceback (most recent call last):
-    TypeError: add_integer() takes from 1 to 2 positional arguments but 3 were given
-
-Test_12 No arguments
---------------------------------
-    >>> add_integer()
-    Traceback (most recent call last):
-    TypeError: add_integer() missing 1 required positional argument: 'a'
-
-Test_13 first argument as string
---------------------------------
-   >>> add_integer("heloo")
     Traceback (most recent call last):
     TypeError: a must be an integer
