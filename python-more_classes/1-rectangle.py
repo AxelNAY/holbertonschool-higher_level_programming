16d8f6fc7c0e0d981a1d5de295af42fb272c44cd
AxelNAY <axelnay@sfr.fr>
2024-05-24 08:52:54 +0000

    Complete the file 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,8 +10,7 @@ class Rectangle:
 
         Args:
             width (int): width of the rectangle
-            height (int): height of the rectangle
-        """
+            height (int): height of the rectangle"""
         self.width = width
         self.height = height
 
@@ -36,7 +35,7 @@ class Rectangle:
         return (self.__height)
 
     @height.setter
-    def width(self, value):
+    def height(self, value):
         """value is the height of the rectangle"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
