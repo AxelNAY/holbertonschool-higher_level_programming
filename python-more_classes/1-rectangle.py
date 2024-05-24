60d65abe076280c54f9594aed46e3369b9256d44
AxelNAY <axelnay@sfr.fr>
2024-05-24 13:30:39 +0000

    put the height before the width

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,23 +11,8 @@ class Rectangle:
         Args:
             width (int): width of the rectangle
             height (int): height of the rectangle"""
-        self.width = width
         self.height = height
-
-    @property
-    def width(self):
-        """current width of the rectangle"""
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """value is the width of the rectangle"""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.width = width
 
     @property
     def height(self):
@@ -41,5 +26,18 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
+        self.__height = value
+
+    @property
+    def width(self):
+        """current width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """value is the width of the rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
