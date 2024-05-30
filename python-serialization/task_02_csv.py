3cc7abe7fd85fb6683b868a2ef32c4a533642c6c
AxelNAY <axelnay@sfr.fr>
2024-05-30 13:22:24 +0000

    Complete the file task_02_csv

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,18 +10,14 @@ def convert_csv_to_json(filename=""):
     Args:
         filename: the name of the CSV file."""
     try:
-        jsonArray = []
+        with open(filename, 'r', encoding='utf-8') as file_csv:
+            csvReader = csv.DictReader(file_csv)
+            data = [row for row in csvReader]
+        with open("data.json", 'w', encoding='utf-8') as file_json:
+            json.dump(data, file_json, indent=4)
 
-        with open(filename, 'r', encoding='utf-8') as csvf:
-            csvReader = csv.DictReader(csvf)
-            for rows in csvReader:
-                jsonArray.append(rows)
-
-        with open("data.json", 'w', encoding='utf-8') as jsonf:
-            jsonString = json.dumps(jsonArray, indent=4)
-            jsonf.write(jsonString)
     except FileNotFoundError:
-        print(f"Erreur : Le fichier {csv_filename} n'a pas été trouvé.")
+        print(f"Erreur : Le fichier {filename} n'a pas été trouvé.")
         return False
     except Exception as e:
         print(f"Erreur : {e}")
