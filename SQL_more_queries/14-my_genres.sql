3a717699d73737fab37e0ea92b52a13546539275
AxelNAY <axelnay@sfr.fr>
2024-06-20 15:12:00 +0200

    Complete all the file in the directory SQL_more_queries

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+SELECT tv_genres.name AS name FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show
+ON tv_show_genres.id = tv_show.id
+WHERE tv_show.title = 'Dexter'
+ORDER BY name ASC;
