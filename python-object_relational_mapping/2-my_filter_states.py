4aec603150a42ccb40632232241f7b8cea02683c
AxelNAY <axelnay@sfr.fr>
2024-06-22 22:05:28 +0200

    task 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,8 +7,12 @@ of hbtn_0e_0_usa where name matches the argument.
 Args:
     username: name of the mysql user.
     password: password of the mysql user.
-    database_name: name of the mysql database.
+    database: name of the mysql database.
     state_name: name of the state searched.
+
+The script connects to a MySQL server running on localhost at port 3306
+and fetches all rows in the `states` table with the name matching the argument,
+sorted in ascending order by `id`.
 """
 
 if __name__ == "__main__":
@@ -16,24 +20,26 @@ if __name__ == "__main__":
     import MySQLdb
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
+    database = sys.argv[3]
     state_name = sys.argv[4]
 
     db = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = username,
-        pwd = password,
-        db_name = database_name
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
     )
 
     cursor = db.cursor()
-    select = """SELECT * FROM states WHERE BINARY
-            name = '{}' ORDER BY id ASC""".format(state_name)
-    cursor.execute(select)
+    query = """SELECT * FROM states WHERE
+    BINARY name = '{}' ORDER BY id ASC""".format(
+        state_name)
+    cursor.execute(query)
     states = cursor.fetchall()
 
     for state in states:
         print(state)
+
     cursor.close()
     db.close()
