e230e7f9e2c74c80880774e3a7a1828590aead32
AxelNAY <axelnay@sfr.fr>
2024-06-22 22:22:44 +0200

    task 4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,6 @@ Args:
     username: name of the mysql user.
     password: password of the mysql user.
     database: name of the mysql database.
-    state_name: name of the state searched.
 """
 
 import sys
@@ -18,7 +17,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host = "localhost",
