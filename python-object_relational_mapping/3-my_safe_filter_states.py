148649cad36b02f82d809000b689b28790256f3c
AxelNAY <axelnay@sfr.fr>
2024-06-22 22:11:17 +0200

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
 """
-Script that takes in arguments and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument. But this time,
-write one that is safe from MySQL injections!
+Se connecte à une base de données MySQL et
+affiche toutes les valeurs de la table "states"
+où le nom correspond à l'argument fourni.
 
-Args:
-    username (str): name of the mysql user.
-    password (str): password of the mysql user.
-    database (str): name of the mysql database.
-    state_name (str): name of the state searched.
+Args :
+    nom_utilisateur (str) : Le nom d'utilisateur de la base de données MySQL.
+    mdp (str) : Le mot de passe de la base de données MySQL.
+    database (str) : Le nom de la base de données.
+    nom_etat (str) : Le nom de l'état à rechercher.
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
+    nom_utilisateur = sys.argv[1]
+    mdp = sys.argv[2]
     database = sys.argv[3]
-    state_name = sys.argv[4]
+    nom_etat = sys.argv[4]
 
     db = MySQLdb.connect(
-        host = "localhost",
-        port = 3306,
-        user = username,
-        pwd = password,
-        db_name = database
+        host="localhost",
+        port=3306,
+        user=nom_utilisateur,
+        passwd=mdp,
+        db=database
     )
 
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name,))
+    cursor.execute(query, (nom_etat,))
 
     states = cursor.fetchall()
 
