58c4d9bdae58b4675e02c036aaf4595075a81acb
AxelNAY <axelnay@sfr.fr>
2024-06-22 22:15:04 +0200

    task 3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """
-Se connecte à une base de données MySQL et
-affiche toutes les valeurs de la table "states"
-où le nom correspond à l'argument fourni.
+This script takes in an argument and displays all values in the states table
+of the database hbtn_0e_0_usa where the name matches the argument.
 
 Args :
-    nom_utilisateur (str) : Le nom d'utilisateur de la base de données MySQL.
-    mdp (str) : Le mot de passe de la base de données MySQL.
-    database (str) : Le nom de la base de données.
-    nom_etat (str) : Le nom de l'état à rechercher.
+    username (str) : name of the mysql user.
+    password (str) : password of the mysql user.
+    database (str) : name of the mysql database.
+    state_name (str) : name of the state searched.
 """
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    nom_utilisateur = sys.argv[1]
-    mdp = sys.argv[2]
+    username = sys.argv[1]
+    password = sys.argv[2]
     database = sys.argv[3]
-    nom_etat = sys.argv[4]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=nom_utilisateur,
-        passwd=mdp,
+        user=username,
+        passwd=password,
         db=database
     )
 
     cursor = db.cursor()
     query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (nom_etat,))
+    cursor.execute(query, (state_name,))
 
     states = cursor.fetchall()
 
